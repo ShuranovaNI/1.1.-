2a5db5aa749273d6ffc8aaae3a35b108b06c8308
Shuranova N <ni.shuranova@gmail.com>
2023-03-29 22:10:14 +0300

    junit4 commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+
+        @Test
+        public void WhenCountingCashbackFromSeveralThousand() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 3000;
+            int actual = service.remain(amount);
+            int expected = 0;
+
+            assertEquals(expected,actual);
+        }
+        @Test
+        public void WhenCashbackIsNotCredited() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 800;
+            int actual = service.remain(amount);
+            int expected = 200;
+
+            assertEquals(expected,actual);
+        }
+        @Test
+        public void WhenExactlyThousandIsSpent() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1000;
+            int actual = service.remain(amount);
+            int expected = 0;
+
+            assertEquals(expected,actual);
+        }
+    }
+
