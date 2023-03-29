4d34df5e2ba95c5a6e1ccc02c7ca9c93afed1bf7
Shuranova N <ni.shuranova@gmail.com>
2023-03-29 21:54:01 +0300

    TestNG commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,39 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import org.testng.Assert.*;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class CashbackHackServiceTest {
+    @Test
+    public void WhenCountingCashbackFromSeveralThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 3000;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void WhenCashbackIsNotCredited() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void WhenExactlyThousandIsSpent() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+}
+
